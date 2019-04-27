93b424362b0361af2c0f45f93ba68a04d3f31fd6
Mathias Mustonen <Mathias.Mustonen@gmail.com>
2019-04-27 13:12:07 +0300

    Foo

--- MoneyTrees/Assets/Scripts/Managers/InputManager.cs
+++ MoneyTrees/Assets/Scripts/Managers/InputManager.cs
@@ -1,6 +1,57 @@
 ﻿using UnityEngine;
 
-public class InputManager : MonoBehaviour
+public class InputManager : Singelton<InputManager>
 {
-   
+    private Collider2D selectedObject;
+    private bool isDragging;
+
+    public bool MouseButtonDown(int index)
+    {
+        return Input.GetMouseButtonDown(index);
+    }
+    public bool MouseButton(int index)
+    {
+        return Input.GetMouseButton(index);
+    }
+    public bool MouseButtonUp(int index)
+    {
+        return Input.GetMouseButtonUp(index);
+    }
+
+    private void Update()
+    {
+        if (MouseButtonDown(0))
+        {
+            isDragging = true;
+
+            RaycastHit2D hit = Physics2D.Raycast(Camera.main.ScreenToWorldPoint(Input.mousePosition), Vector2.zero);
+
+            if (hit.collider != null)
+            {
+                selectedObject = hit.collider;
+            }
+        }
+
+        if (MouseButton(0))
+        {
+            if (isDragging)
+            {
+                MoveCollider();
+            }
+        }
+
+        if (MouseButtonUp(0))
+        {
+            isDragging = false;
+            selectedObject = null;
+        }
+    }
+
+    private void MoveCollider()
+    {
+        if(selectedObject != null)
+        {
+            selectedObject.transform.position = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+        }
+    }
 }
--- MoneyTrees/Assets/Scripts/Managers/UIManager.cs
+++ MoneyTrees/Assets/Scripts/Managers/UIManager.cs
@@ -1,6 +1,6 @@
 ﻿using UnityEngine;
 
-public class UIManager : MonoBehaviour
+public class UIManager : Singelton<UIManager>
 {
-
+   
 }
